9311877425363fd9ecd3aff79475c3bda10b4635
hferr404 <ferrache.h@gmail.com>
2021-02-22 15:40:27 +0100

    premiere version index

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CDN bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <title>PROJET GIT</title>
+</head>
+    
+    <body> 
+        <div class="container" style="min-height: 70vh;">
+        <h1 class="display-4 text-center bg-dark text-white rounded shadow-lg my-5">PROJET GIT TEST</h1>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100" src="https://picsum.photos/200/300" alt="First slide" style="width: 220px; height: 200px">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="https://picsum.photos/200/300" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="https://picsum.photos/200/300" alt="Third slide">
+                </div>
+            </div>
+            </div>
+                
+            <h4 class="display-4"></h4>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                        The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+                        content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+                        and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                        Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        </div>
+
+        <footer class="bg-dark text-white text-center">
+            &copy; FH - DANS LE LIN LEGACY - 2021
+        </footer>
+    </body>
+</html>     
